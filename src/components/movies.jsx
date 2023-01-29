d6600cd7e560e6a4170abd319d6c4a74afa952ea
GhaithAlBasha <ghaith.albasha@yahoo.com>
2023-01-29 22:31:17 +0300

    Vidly app files

---
+++ src/components/movies.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { getMovies } from '../services/fakeMovieService';
+
+class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+
+  handleDelete = movie => {};
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Movies;
