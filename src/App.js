9bae50f2030259013b27b4b11a0f058c7cf9593b
GhaithAlBasha <ghaith.albasha@yahoo.com>
2023-02-23 11:04:31 +0300

    Routing Exercise

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
+import { Route, Redirect, Switch } from 'react-router-dom';
 import Movies from './components/movies';
+import NavBar from './components/navBar';
+import Customers from './components/customers';
+import Rentals from './components/rentals';
+import NotFound from './components/notFound';
 import './App.css';
+import MovieForm from './components/movieForm';
 
 class App extends Component {
     render() {
         return (
-            <main className="container">
-                <Movies />
-            </main>
+            <>
+                <NavBar />
+                <main className="container">
+                    <Switch>
+                        <Route path="/movies/:id" component={MovieForm} />
+                        <Route path="/movies" component={Movies}></Route>
+                        <Route path="/customers" component={Customers}></Route>
+                        <Route path="/rentals" component={Rentals}></Route>
+                        <Route path="/not-found" component={NotFound}></Route>
+                        <Redirect from="/" exact to="/movies" />
+                        <Redirect to="/not-found" />
+                    </Switch>
+                </main>
+            </>
         );
     }
 }
--- src/components/customers.jsx
+++ src/components/customers.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Customer = props => {
-    return <h1>Customer</h1>;
+const Customers = props => {
+    return <h1>Customers</h1>;
 };
 
-export default Customer;
+export default Customers;
